3fdc06f7a443f21e842697447a54746fd9c45c24
wickkan <75600207+wickkan@users.noreply.github.com>
2022-10-04 16:51:48 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,9 @@ vegaEmbed("#nyc_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
+
+var vg_2 = "nights_avail.json";
+vegaEmbed("#nights_avail", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
